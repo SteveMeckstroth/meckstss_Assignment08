179560679e3b00325f60250d69f2fa0e034d471d
Stephen Meckstroth <meckstss@gmail.com>
2018-04-03 17:31:55 -0400

    Initial commit

---
+++ meckstss_Assignment08/src/meckstss_Assignment08/Main.java
@@ -0,0 +1,20 @@
+/**
+ * Assignment 08
+ * @author Stephen Meckstroth
+ * Description: An assignment to implement a spellchecker method
+ * Due Date: Apr 3, 2018
+ * Course: IT2045 Section 001
+ * email: meckstss@mail.uc.edu
+ * Citations: N/A
+ */
+
+package meckstss_Assignment08;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
